2099fa603b3043652f18fa3c879b29170fde9983
uniquecourage <garrick@linjinlongdeMacBook-Pro.local>
2015-09-01 14:36:24 +0800

    20150901公司上傳

---
+++ PageBasedSelf/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  PageBasedSelf
+//
+//  Created by 林金龍 on 2015/9/1.
+//  Copyright (c) 2015年 林金龍. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
